c83b7120c04be4ae682201bc1662a49c738dcefa
Adriano Fantini <adr.fantini@gmail.com>
2019-05-07 13:40:13 +0200

    Config bandiera added

--- config_bandiera.R
+++ config_bandiera.R
@@ -1,1 +1,6 @@
- 
+input_folder = "data/"
+input_name = 'dati-orari-Trieste molo F.lli Bandiera-'
+location_name = 'Molo Fratelli Bandiera, Trieste, Italy'
+location_output_name = 'bandiera'
+location = c(13.7516938, 45.649985) # lat-lon
+output_folder = 'plots/'
